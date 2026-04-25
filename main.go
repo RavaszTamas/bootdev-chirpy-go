4a85fae1f4b0041da0d483d4ddc2296f11b6fe38
RavaszTamas <ravaszt88@gmail.com>
2026-04-25 15:14:03 +0300

    logo

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import "net/http"
 
 func main() {
 	mux := http.NewServeMux()
+
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+
 	server := http.Server{
 		Handler: mux,
 		Addr:    "localhost:8080",
