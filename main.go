f77506d4016be2610ad0fd4dbac62e51475f3af4
RavaszTamas <ravaszt88@gmail.com>
2026-04-27 20:30:27 +0300

    Implement middleware for metrics tracking and add health and metrics endpoints

--- main.go
+++ main.go
@@ -1,11 +1,46 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main() {
 	mux := http.NewServeMux()
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
+	mux.Handle("/app/", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write([]byte("OK"))
+	})
+
+	mux.HandleFunc("GET /metrics", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write([]byte(fmt.Sprintf("Hits: %d", apiCfg.fileserverHits.Load())))
+	})
+
+	mux.HandleFunc("POST /reset", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write([]byte("RESET"))
+	})
 
 	server := http.Server{
 		Handler: mux,
